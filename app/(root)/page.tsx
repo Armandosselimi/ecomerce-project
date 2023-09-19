39227a8e1dd02a580cbecf07c17cbbdb059c9872
armandosselimi <armandosselimi@gmail.com>
2023-09-19 18:46:52 +0200

    implementing authentication with clerck

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button";
+import {UserButton} from "@clerk/nextjs";
+
+const SetupPage= () => {
+    return (
+       <div className='p-5'>
+           <UserButton afterSignOutUrl="/"/>
+       </div>
+    )
+}
+
+export default SetupPage
