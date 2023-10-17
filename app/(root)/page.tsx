a6328884c851c8548ebfdee29975f9ce8863272f
armandosselimi <armandosselimi@gmail.com>
2023-10-17 17:38:01 +0200

    added database tools

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,18 +4,18 @@ import {useStoreModal} from "@/hooks/use-store-modal";
 import {useEffect} from "react";
 
 const SetupPage = () => {
-    const onOpen = useStoreModal((state)=>state.onOpen)
-    const isOpen = useStoreModal((state)=>state.isOpen)
+    const onOpen = useStoreModal((state) => state.onOpen)
+    const isOpen = useStoreModal((state) => state.isOpen)
 
-useEffect(()=>{
-    if (!isOpen){
-        onOpen()
-    }
-},[isOpen,onOpen])
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen()
+        }
+    }, [isOpen, onOpen])
 
     return (
         <div className='p-5'>
-       Root Page
+            Root Page
         </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import {ClerkProvider} from "@clerk/nextjs";
 import React from "react";
 
 import {ModalProvider} from "@/providers/modal-provider";
+import {ToastProvider} from "@/providers/toast-provider";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <ClerkProvider>
             <html lang="en">
             <body className={inter.className}>
+            <ToastProvider/>
             <ModalProvider/>
             {children}
             </body>
